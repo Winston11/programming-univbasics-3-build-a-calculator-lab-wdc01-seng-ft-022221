3f94f5ac9cbc3b2dd59e5a11b0732c5affde4e99
Winston McGriff <winstonmcgriff27@gmail.com>
2021-02-02 02:49:31 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,21 +1,21 @@
 def addition(num1, num2)
-
+addition = num2 + num1
 end
 
 def subtraction(num1, num2)
-
+subtraction = num2 - num1
 end
 
 def division(num1, num2)
-
+division = num2/num1
 end
 
 def multiplication(num1, num2)
-
+multiplication = num1*num2
 end
 
 def modulo(num1, num2)
-
+modulo = num2%num1
 end
 
 def square_root(num)
